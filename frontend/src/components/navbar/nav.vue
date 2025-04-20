076fb688fc122b1ba483461dcd4d366484f78332
Felobateer <felobateersamy01@gmail.com>
2025-04-20 22:34:57 +0200

    add assets and start navbar

---
+++ frontend/src/components/navbar/nav.vue
@@ -0,0 +1,31 @@
+<script>
+import navModal from './navModal.vue'
+// import logo from '@/assets/Gotel-logo-fedbbdcb.jpg'
+
+</script>
+
+<template>
+    <nav class="w-100">
+        <div>
+            <img  src="../../assets/Gotel-logo-fedbbdcb.jpg" alt="Gotel" class="logo"/>
+        </div>
+        <NavModal></NavModal>   
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        position: fixed;
+        top: 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 20px;
+        background: transparent;
+    }
+
+    .logo {
+        width: auto;
+        height: 80px;
+    }
+</style>
