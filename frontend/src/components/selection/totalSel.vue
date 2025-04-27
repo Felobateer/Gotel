7be0f6451c502a1c32617ce5b02c3ae05412e01e
Felobateer <felobateersamy01@gmail.com>
2025-04-27 23:30:29 +0200

    add selections

---
+++ frontend/src/components/selection/totalSel.vue
@@ -0,0 +1,11 @@
+<script>
+    import { primarySel} from './primarySel.vue'
+    import { secondarySel} from './secondarySel.vue'
+</script>
+
+<template>
+    <div class="totalSel">
+        <primarySel/>
+        <secondarySel/>
+    </div>
+</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Hotel from './views/Hotel.vue'
 import Contact from './views/Contact.vue'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
@@ -9,6 +10,7 @@ const routes = [
     {path: '/', component: Home},
     {path: '/about', component: About},
     {path: '/contact', component: Contact},
+    {path: '/hotels', component: Hotel},
     {path: '/login', component: Login},
     {path: '/register', component: Register},
 ];
