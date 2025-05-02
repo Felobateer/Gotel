f03c9d048925653f0866e7416608af4129304f60
Felobateer <felobateersamy01@gmail.com>
2025-05-02 14:18:42 +0200

    update input

--- frontend/src/components/selection/inputs/countInput.vue
+++ frontend/src/components/selection/inputs/countInput.vue
@@ -2,6 +2,7 @@
 </script>
 
 <template>
+<!-- this is the count input for adults, children, and rooms required by the user -->
     <div class="inputContainer">
         <input type="text" />
     </div>
