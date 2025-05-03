4c52e825ef2bc62c69209d46b3d61228db39a5b5
Felobateer <felobateersamy01@gmail.com>
2025-05-03 10:21:06 +0200

    add secondary selection

---
+++ frontend/src/components/selection/secondarySel.vue
@@ -0,0 +1,11 @@
+<script>
+    import priceInput from './priceInput.vue'
+    import rateInput from './rateInput.vue'    
+</script>
+
+<template>
+    <section>
+        <priceInput />
+        <rateInput />
+</section>
+</template>
