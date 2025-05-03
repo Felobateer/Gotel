916825090e43b8a865dda74d34c3c0c462c2470b
Felobateer <felobateersamy01@gmail.com>
2025-05-03 10:20:00 +0200

    add primary selection

--- frontend/src/components/selection/primarySel.vue
+++ frontend/src/components/selection/primarySel.vue
@@ -1,6 +1,15 @@
 <script>
+import cityInput from './cityInput.vue'
+import countInput from './countInput.vue'
+import dateInput from './dateInput.vue'
 </script>
 
 <template>
-    
+    <section>
+        <h1>Primary Selection</h1>
+        <cityInput />
+        <countInput />
+        <dateInput />
+        <p>Primary Selection Component</p>
+    </section>
 </template>
