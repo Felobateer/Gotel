02050753bd20d3f3ec633a149f7894218de353a4
Felobateer <felobateersamy01@gmail.com>
2025-07-02 02:31:38 +0200

    add authorizations

--- backend/api/hotel/views.py
+++ backend/api/hotel/views.py
@@ -9,11 +9,17 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
+#Custom Permission to allow only hotel owners to modify, create, or delete hotels
+class IsModeratorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated and request.user.is_moderator
 
 class HotelListCreate(generics.ListCreateAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsModeratorOrReadOnly]
     pagination_class = StandardResultsSetPagination
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['name', 'price', 'rating']
@@ -22,11 +28,6 @@ class HotelListCreate(generics.ListCreateAPIView):
 class HotelDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsModeratorOrReadOnly]
     lookup_field = 'pk'
     
-    def get_object(self):
-        obj = super().get_object()
-        if obj.user != self.request.user:
-            self.permission_denied(self.request)
-        return obj
--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -22,7 +22,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
-    is_host = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_mod = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(upload_to='profile_images/', null=True, blank=True)
 
